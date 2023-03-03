a7ed2145abbff2865a1686ca161beb6ae0ec5c5f
Airidas Juska <airidas665@gmail.com>
2023-03-03 11:51:40 +0200

    task 1

---
+++ App.js
@@ -0,0 +1,11 @@
+
+//TASK 1
+function compare(a, b) {
+    a > b ? console.log(`A is bigger (${a})`) : a==b ? console.log(`They are equal (${a}) (${b})`) : console.log(`B is bigger (${b})`)
+}
+// compare(5, 5)
+
+
+//TASK 2
+
+
