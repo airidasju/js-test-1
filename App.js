d9ec44a5540934a4f5a9c1ba86fade641a59553a
Airidas Juska <airidas665@gmail.com>
2023-03-03 13:24:36 +0200

    task 10

--- App.js
+++ App.js
@@ -74,46 +74,72 @@ function task7() {
 
 //TASK 8
 function task8(a, b) {
-    if (
-        (typeof a == 'number' && typeof b == 'number') ||
-        (typeof a == 'object' && typeof b == 'object')
-      ) {
-        if(typeof a == "number") {
-          const sum = a + b
-          console.log(sum)
-          return sum % 2 == 0 ? sum : console.log("Sum is not even")
-        } else {
-          const sum = a.length + b.length
-          console.log(sum)
-          return sum % 2 == 0 ? sum : console.log("Sum is not even")
-        }
-      } else {
-        console.log(`They are not of the same type`, typeof a, typeof b);
-      }
+  if (
+    (typeof a == 'number' && typeof b == 'number') ||
+    (typeof a == 'object' && typeof b == 'object')
+  ) {
+    if (typeof a == 'number') {
+      const sum = a + b;
+      console.log(sum);
+      return sum % 2 == 0 ? sum : console.log('Sum is not even');
+    } else {
+      const sum = a.length + b.length;
+      console.log(sum);
+      return sum % 2 == 0 ? sum : console.log('Sum is not even');
+    }
+  } else {
+    console.log(`They are not of the same type`, typeof a, typeof b);
+  }
 }
 // task8(5, 5);
 
-
-
 //TASK 9
 function task9(number) {
-    let isPrime = true
-    if(number < 2) {
-        console.log("Number is less than 2")
-        return number
-    }
-    for (let i = 2; i < number; i++) {
-        if (number % i == 0) {
-            isPrime = false;
-            break;
-        }
-    }
-    if (isPrime) {
-        console.log(`${number} is a prime number`);
-        return number
-    } else {
-        console.log(`${number} is a not prime number`);
-        return number
+  let isPrime = true;
+  if (number < 2) {
+    console.log('Number is less than 2');
+    return number;
+  }
+  for (let i = 2; i < number; i++) {
+    if (number % i == 0) {
+      isPrime = false;
+      break;
     }
   }
-  task9(45);
+  if (isPrime) {
+    console.log(`${number} is a prime number`);
+    return number;
+  } else {
+    console.log(`${number} is a not prime number`);
+    return number;
+  }
+}
+//   task9(45);
+
+//TASK 10
+function task10(phone) {
+  const result = [];
+  let count = 0;
+  for (let i = 0; i < 10; i++) {
+    if (typeof phone[i] !== 'number') {
+      console.log('wrong input', phone[i]);
+      return;
+    }
+  }
+  const template = '(XXX) XXX-XXXX'.split('');
+  console.log(template);
+  for (let i = 0; i < template.length; i++) {
+    if (template[i] !== 'X') {
+      result.push(template[i]);
+      count++;
+    } else {
+      result.push(phone[i - count]);
+    }
+  }
+  const phoneFinished = result.join('');
+  console.log(phoneFinished);
+  return phoneFinished;
+}
+// task10([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+
+// Nelabai elegant kodas, bet veikia :D
